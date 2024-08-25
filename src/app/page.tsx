ece3d2ee6b1a05ec5547d2d078d3fc10f80d7598
LoboDev <mauriciolav@gmail.com>
2024-08-25 14:26:05 -0300

    act

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,44 +8,38 @@ export default function Home() {
   const [isFadingOut, setIsFadingOut] = useState(false);
   const [animationKey, setAnimationKey] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
-  const audioRef = useRef(null);
+  const audioRef = useRef<HTMLAudioElement | null>(null); // Asegúrate de especificar el tipo
 
   const fetchFrase = async () => {
     try {
       const response = await fetch("http://localhost/frases/public/frase-aleatoria");
       const data = await response.json();
-      return data.frase;
+      setIsFadingOut(true);
+      setTimeout(() => {
+        setFrase(data.frase);
+        setIsFadingOut(false);
+        setAnimationKey(prevKey => prevKey + 1);
+      }, 500);
     } catch (error) {
       console.error("Error al obtener la frase:", error);
-      return "";
     }
   };
 
-  const updateFrase = async () => {
-    setIsFadingOut(true);
-
-    // Esperar a que termine el fadeOut
-    setTimeout(async () => {
-      const nuevaFrase = await fetchFrase();
-      setFrase(nuevaFrase);
-      setIsFadingOut(false);
-      setAnimationKey(prevKey => prevKey + 1);
-    }, 1000); // Duración del fadeOut en milisegundos
-  };
-
   useEffect(() => {
-    updateFrase();
-    const interval = setInterval(updateFrase, 10000); // Cambia cada 10 segundos
+    fetchFrase();
+    const interval = setInterval(fetchFrase, 10000);
     return () => clearInterval(interval);
   }, []);
 
   const toggleMusic = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
     }
-    setIsPlaying(!isPlaying);
   };
 
   return (
@@ -54,14 +48,14 @@ export default function Home() {
         <h1
           key={animationKey}
           className={`text-2xl md:text-3xl lg:text-5xl font-bold leading-10 md:leading-[1.5] lg:leading-[1.6] text-slate-300
-            ${isFadingOut ? "animate__animated animate__fadeOut" : "animate__animated animate__fadeIn"} animate__duration-1s`}
+            ${isFadingOut ? "animate__animated animate__fadeOut" : "animate__animated animate__fadeIn"} animate__delay-1s`}
         >
           {frase}
         </h1>
       </div>
 
-      <audio ref={audioRef} src="music.mp3" loop />
-
+      <audio ref={audioRef} src="/path/to/your/instrumental-music.mp3" loop />
+      
       <button
         onClick={toggleMusic}
         className="absolute bottom-4 right-4 p-3 bg-gray-800 text-white rounded-full shadow-lg hover:bg-gray-700 focus:outline-none"
